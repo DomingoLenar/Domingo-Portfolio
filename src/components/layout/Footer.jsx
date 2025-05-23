6aef6f6733b466fdc8bb6dd293da44593cca09e3
Lenar <lenardomingo14@gmail.com>
2025-05-24 01:16:19 +0800

    + Layout Structure Changes

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,11 +1,10 @@
-import Link from 'next/link'
-
-export default function Navbar() {
-  return (
+export default function Footer() {
+return (
     <nav>
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/projects">Projects</Link>
+      <Link href="/dashboard">Dashboard</Link>
     </nav>
   )
 }
