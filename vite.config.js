ee3c6b71ad2b922d48c3a3fb2e150b2eac92b90f
Lenar <lenardomingo14@gmail.com>
2026-07-14 00:18:25 +0800

    + Deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Domingo-Portfolio/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
